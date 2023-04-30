b697c63ee8e560191383d3e79275895eb3d3aa82
shafiekdavids <shafiekd@gmail.com>
2023-04-30 21:14:14 +0200

    ESLINT

--- server/utils/config.js
+++ server/utils/config.js
@@ -11,11 +11,17 @@ function createDatabaseUrl() {
 	if (process.env.DATABASE_URL) {
 		return process.env.DATABASE_URL;
 	}
-	const host = process.env.DB_HOST ?? "localhost";
-	const name = process.env.DB_NAME ?? "cyf";
-	const password = process.env.DB_PASS ?? process.env.DB_PASSWORD ?? "";
+	const host =
+		process.env.DB_HOST ??
+		"dpg-ch6fa5o2qv26p1858j7g-a.frankfurt-postgres.render.com";
+	const name = process.env.DB_NAME ?? "cyf_ar6r";
+	const password =
+		process.env.DB_PASS ??
+		process.env.DB_PASSWORD ??
+		"iuA1JaO3O3Cq5kA3f7NmyUlBHKUvQebq";
 	const port = process.env.DB_PORT ?? "5432";
-	const username = process.env.DB_USER ?? process.env.DB_USERNAME ?? "";
+	const username =
+		process.env.DB_USER ?? process.env.DB_USERNAME ?? "cyf_ar6r_user";
 	const userinfo = `${username}:${password}`;
 	return `postgres://${
 		userinfo !== ":" ? `${userinfo}@` : ""
